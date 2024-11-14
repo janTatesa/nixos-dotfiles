7dca49a2fb22160dc8806c5b613e6700c5f9726a
Tadeas Uradnik <taduradnik@gmail.com>
2024-11-14 15:50:24 +0100

    194 current  2024-11-14 15:50:21  24.05.20241112.689fed1  6.10.1-zen1                          *

--- home-manager/discord.nix
+++ home-manager/discord.nix
@@ -5,26 +5,10 @@
   ...
 }: {
   home = {
-    # file."${config.xdg.configHome}/discord/settings.json" = {
-    #   text = builtins.toJSON {
-    #     SKIP_HOST_UPDATE = true;
-    #     DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
-    #     MIN_WIDTH = 940;
-    #     MIN_HEIGHT = 500;
-    #     openasar = {
-    #       setup = true;
-    #       cmdPreset = "balanced";
-    #       css = "${builtins.readFile catppuccin-discord} ${builtins.readFile ./discord.css} *, *::before{font-family: Ubuntu !important;}";
-    #     };
-    #     chromiumSwitches = {};
-    #     MINIMIZE_TO_TRAY = false;
-    #     OPEN_ON_STARTUP = false;
-    #     IS_MAXIMIZED = true;
-    #     IS_MINIMIZED = false;
-    #   };
-    #   recursive = true;
-    #   force = true;
-    # };
+    file = {
+      ".config/vesktop/themes/catppuccin.css".text = "${catppuccin-discord}";
+      ".config/vesktop/themes/purecord.css".source = ./discord.css;
+    };
     packages = [
       pkgs.vesktop
     ];
