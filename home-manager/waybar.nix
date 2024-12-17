d54bf0d9686b0403d2ba2e095dc6e2eec4a49618
Tatesa Uradnik <taduradnik@proton.me>
2024-12-17 09:34:44 +0100

    27 current  2024-12-17 09:34:41  24.11.20241216.3945713        6.12.2-zen1                          *

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -2,84 +2,82 @@
   programs.waybar = {
     enable = true;
     style = builtins.readFile ./waybar.css;
-    settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        output = ["eDP-1"];
-        margin-bottom = 10;
-        modules-left = ["sway/workspaces" "group/indicators"];
-        modules-center = ["clock"];
-        modules-right = ["sway/mode" "sway/language" "custom/waybar-mpris"];
-        network = {
-          interface = "wlp0s20f3";
-          format-wifi = "<span color='#cba6f7'>{signalStrength}%</span> {essid}";
-          format-disconnected = "<span color='#f38ba8'>󰖪</span>No Network";
-          tooltip = false;
+    settings.mainBar = {
+      layer = "top";
+      position = "top";
+      output = ["eDP-1"];
+      margin-bottom = 10;
+      modules-left = ["sway/workspaces" "group/indicators"];
+      modules-center = ["clock"];
+      modules-right = ["sway/mode" "sway/language" "custom/waybar-mpris"];
+      network = {
+        interface = "wlp0s20f3";
+        format-wifi = "<span color='#cba6f7'>{signalStrength}%</span> {essid}";
+        format-disconnected = "<span color='#f38ba8'>󰖪</span>No Network";
+        tooltip = false;
+      };
+      "group/indicators" = {
+        modules = ["network" "battery" "backlight" "pulseaudio"];
+        orientation = "horizontal";
+      };
+      "sway/workspaces" = {
+        disable-scroll = true;
+        sort-by-number = true;
+        disable-click = true;
+        format = "{icon}";
+        format-icons = {
+          "1" = "󰇧";
+          "2" = "";
+          "3" = "";
+          "4" = "4";
+          "5" = "5";
+          "6" = "6";
+          "7" = "7";
+          "8" = "8";
+          "9" = "9";
+          "0" = "0";
         };
-        "group/indicators" = {
-          modules = ["network" "battery" "backlight" "pulseaudio"];
-          layout = "horizontal";
+        persistent-workspaces = {
+          "1" = [];
+          "2" = [];
+          "3" = [];
         };
-        "sway/workspaces" = {
-          disable-scroll = true;
-          sort-by-number = true;
-          disable-click = true;
-          format = "{icon}";
-          format-icons = {
-            "1" = "󰇧";
-            "2" = "";
-            "3" = "";
-            "4" = "4";
-            "5" = "5";
-            "6" = "6";
-            "7" = "7";
-            "8" = "8";
-            "9" = "9";
-            "0" = "0";
-          };
-          persistent-workspaces = {
-            "1" = [];
-            "2" = [];
-            "3" = [];
-          };
-        };
-        "sway/mode".tooltip = false;
-        "custom/waybar-mpris" = {
-          return-type = "json";
-          max-length = 50;
-          exec = "${pkgs.waybar-mpris}/bin/waybar-mpris --autofocus --order \"ARTIST:TITLE\"";
-          escape = true;
-          tooltip = false;
-        };
-        "sway/language" = {
-          format = "{shortDescription}";
-          tooltip = false;
-        };
+      };
+      "sway/mode".tooltip = false;
+      "custom/waybar-mpris" = {
+        return-type = "json";
+        max-length = 50;
+        exec = "${pkgs.waybar-mpris}/bin/waybar-mpris --autofocus --order \"ARTIST:TITLE\"";
+        escape = true;
+        tooltip = false;
+      };
+      "sway/language" = {
+        format = "{shortDescription}";
+        tooltip = false;
+      };
 
-        clock = {
-          interval = 60;
-          format = "{:%a %d/%b %I:%M}";
-        };
-        backlight = {
-          device = "intel_backlight";
-          format = "<span color='#cba6f7'>{icon}</span> {percent}%";
-          format-icons = ["" "" "" "" "" "" "" "" ""];
-          disable-scroll = true;
-        };
-        pulseaudio = {
-          format = "<span color='#cba6f7'>{icon}</span> {volume}%";
-          format-muted = "<span color='#f38ba8'>󰖁</span> {volume}%";
-          tooltip = false;
-          format-icons.default = ["󰕿" "󰕿" "󰖀" "󰖀" "󰖀" "󰕾" "󰕾" "󰕾"];
-          disable-scroll = true;
-        };
-        battery = {
-          tooltip = false;
-          format = "<span color='#cba6f7'>{icon}</span> {capacity}%";
-          format-icons = ["" "" "" "" "" "" "" "" "" ""];
-          format-charging = "<span color='#cba6f7'>{icon} Charging</span> {capacity}%";
-        };
+      clock = {
+        interval = 60;
+        format = "{:%a %d/%b %I:%M}";
+      };
+      backlight = {
+        device = "intel_backlight";
+        format = "<span color='#cba6f7'>{icon}</span> {percent}%";
+        format-icons = ["" "" "" "" "" "" "" "" ""];
+        disable-scroll = true;
+      };
+      pulseaudio = {
+        format = "<span color='#cba6f7'>{icon}</span> {volume}%";
+        format-muted = "<span color='#f38ba8'>󰖁</span> {volume}%";
+        tooltip = false;
+        format-icons.default = ["󰕿" "󰕿" "󰖀" "󰖀" "󰖀" "󰕾" "󰕾" "󰕾"];
+        disable-scroll = true;
+      };
+      battery = {
+        tooltip = false;
+        format = "<span color='#cba6f7'>{icon}</span> {capacity}%";
+        format-icons = ["" "" "" "" "" "" "" "" "" ""];
+        format-charging = "<span color='#cba6f7'>{icon} Charging</span> {capacity}%";
       };
     };
   };
