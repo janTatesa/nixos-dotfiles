eae857de0d653eea299b1c71edec10cf18d67b71
Tatesa Uradnik <taduradnik@proton.me>
2024-12-30 09:48:56 +0100

    38 current  2024-12-30 09:48:52  24.11.20241228.9f94733        6.12.2-zen1                          *

--- home-manager/helix/keybinds.nix
+++ home-manager/helix/keybinds.nix
@@ -7,10 +7,10 @@
       right = "no_op";
     };
     insert = {
-      C-right = "move_next_word_start";
-      C-left = "move_prev_word_end";
-      C-del = "delete_word_forward";
-      C-backspace = "delete_word_backward";
+      left = "no_op";
+      down = "no_op";
+      up = "no_op";
+      right = "no_op";
     };
 
     select = {
