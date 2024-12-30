2dc075a74ee07b6af5f2ea2e97ecdb303bcfeeff
Tatesa Uradnik <taduradnik@proton.me>
2024-12-30 09:55:33 +0100

    39 current  2024-12-30 09:55:30  24.11.20241228.9f94733        6.12.2-zen1                          *

--- home-manager/helix/keybinds.nix
+++ home-manager/helix/keybinds.nix
@@ -1,23 +1,20 @@
 {...}: {
   programs.helix.settings.keys = {
     normal = {
-      left = "no_op";
-      down = "no_op";
-      up = "no_op";
-      right = "no_op";
+      C-right = "move_next_word_start";
+      C-left = "move_prev_word_end";
     };
+
     insert = {
-      left = "no_op";
-      down = "no_op";
-      up = "no_op";
-      right = "no_op";
+      C-right = "move_next_word_start";
+      C-left = "move_prev_word_end";
+      C-del = "delete_word_forward";
+      C-backspace = "delete_word_backward";
     };
 
     select = {
-      left = "no_op";
-      down = "no_op";
-      up = "no_op";
-      right = "no_op";
+      C-right = "extend_next_word_start";
+      C-left = "extend_prev_word_end";
     };
   };
 }
