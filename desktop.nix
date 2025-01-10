c6abceb77a2bbc210c52dacc8cc66a2c8e691c66
Tatesa Uradnik <taduradnik@proton.me>
2025-01-10 13:24:07 +0100

    46 current  2025-01-10 13:24:02  24.11.20250106.3f0a8ac        6.12.2-zen1                          *

--- desktop.nix
+++ desktop.nix
@@ -2,7 +2,11 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [xdg-desktop-portal-wlr xdg-desktop-portal-gtk];
-    config.common.default = "*";
+    config.sway = {
+      default = "gtk";
+      "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+      "org.freedesktop.impl.portal.Screenshot" = "wlr";
+    };
   };
   services.gnome.gnome-keyring.enable = true;
   programs = {
