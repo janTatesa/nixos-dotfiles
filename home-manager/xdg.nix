cb0684e450c61bfeb1a6298fb4e474043b30611e
Tatesa Uradnik <taduradnik@proton.me>
2025-02-14 11:16:38 +0100

    79 current  2025-02-14 11:16:30  24.11.20250212.0ff09db        6.12.10-zen1                          *

--- home-manager/xdg.nix
+++ home-manager/xdg.nix
@@ -2,11 +2,11 @@
   xdg = {
     mimeApps.defaultApplications = {
       "inode/directory" = "kitty.desktop";
-      "text/html" = "librewolf.desktop";
-      "x-scheme-handler/http" = "librewolf.desktop";
-      "x-scheme-handler/https" = "librewolf.desktop";
-      "x-scheme-handler/about" = "librewolf.desktop";
-      "x-scheme-handler/unknown" = "librewolf.desktop";
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
     };
 
     userDirs = {
@@ -16,9 +16,9 @@
       publicShare = null;
       desktop = null;
       templates = null;
+      documents = null;
 
       # Small letters are easier to type
-      documents = "${config.home.homeDirectory}/documents";
       download = "${config.home.homeDirectory}/downloads";
       music = "${config.home.homeDirectory}/music";
       pictures = "${config.home.homeDirectory}/pictures";
