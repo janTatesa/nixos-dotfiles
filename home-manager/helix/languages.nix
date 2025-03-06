ef554155469f6e87146c16bdc177828e6e4ccff0
Tatesa Uradnik <taduradnik@proton.me>
2025-03-06 17:24:00 +0100

    102 current  2025-03-06 17:23:51  24.11.20250304.6af28b8        6.12.10-zen1                          *

--- home-manager/helix/languages.nix
+++ home-manager/helix/languages.nix
@@ -12,6 +12,9 @@
         formatter.command = "${pkgs.astyle}/bin/astyle";
       }
     ];
-    language-server.rust-analyzer.config.check.command = "clippy";
+    language-server.rust-analyzer.config = {
+      check.command = "clippy";
+      completion.excludeTraits = ["color_eyre::owo_colors::OwoColorize" "crossterm::style::Stylize"];
+    };
   };
 }
