e179dd814cb1301e9bbce1aa2d0252304f974efb
Tatesa Uradnik <taduradnik@proton.me>
2025-04-14 13:40:02 +0200

    154 current  2025-04-14 13:39:53  24.11.20250412.26d499f        6.12.10-zen1                          *

--- home-manager/helix/keybinds.nix
+++ home-manager/helix/keybinds.nix
@@ -5,10 +5,10 @@
       C-left = "move_prev_word_end";
       C-del = "delete_word_forward";
       C-backspace = "delete_word_backward";
-      C-w = "buffer_close";
+      C-w = ":buffer_close";
       C-t = "file-picker";
-      C-pageup = "buffer-previous";
-      C-pagedown = "buffer-previous";
+      C-pageup = ":buffer-previous";
+      C-pagedown = ":buffer-next";
     };
     insert = normal;
     select = normal;
