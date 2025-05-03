1cae68153a4ecad7530e15d00cc876efbf84ae4d
Tatesa Uradnik <taduradnik@proton.me>
2025-05-03 09:41:07 +0200

    183 current  2025-05-03 09:41:05  24.11.20250502.bf3287d  6.14.4-zen1                           *

--- home-manager-shared/waybar.nix
+++ home-manager-shared/waybar.nix
@@ -7,7 +7,7 @@
   programs.waybar = {
     enable = true;
     style =
-      ":root{font-family: '${builtins.elemAt system-config.fonts.fontconfig.defaultFonts.sansSerif 0}', '${builtins.elemAt system-config.fonts.fontconfig.defaultFonts.monospace 0}';font-size: ${builtins.toString font-size}px}"
+      "*{font-family: '${builtins.elemAt system-config.fonts.fontconfig.defaultFonts.sansSerif 0}', '${builtins.elemAt system-config.fonts.fontconfig.defaultFonts.monospace 0}';font-size: ${builtins.toString font-size}px}"
       + builtins.readFile ../assets/waybar.css;
     settings.mainBar = {
       margin-bottom = 10;
