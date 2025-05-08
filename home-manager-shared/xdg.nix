920c29633993432b0529a264cbbf588ad83fb4c3
Tatesa Uradnik <taduradnik@proton.me>
2025-05-08 16:13:49 +0200

    190 current  2025-05-08 16:13:47  24.11.20250506.1d3aeb5  6.14.4-zen1                           *

--- home-manager-shared/xdg.nix
+++ home-manager-shared/xdg.nix
@@ -16,13 +16,15 @@
       publicShare = null;
       templates = null;
       documents = null;
+      videos = null;
 
+      # Some apps support creating .desktop files
       desktop = "${config.xdg.dataHome}/applications";
+
       # Small letters are easier to type
       download = "${config.home.homeDirectory}/downloads";
       music = "${config.home.homeDirectory}/music";
       pictures = "${config.home.homeDirectory}/pictures";
-      videos = "${config.home.homeDirectory}/videos";
     };
   };
 }
