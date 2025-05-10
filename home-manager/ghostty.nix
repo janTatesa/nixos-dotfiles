fb78395b1ec51d5247bc5bbf9ebf4e83eaef61cf
Tatesa Uradnik <taduradnik@proton.me>
2025-05-10 08:22:02 +0200

    199 current  2025-05-10 08:22:00  24.11.20250506.1d3aeb5  6.14.4-zen1                           *

--- home-manager/ghostty.nix
+++
@@ -1,9 +0,0 @@
-{font-size, system-config, ...}: {
-  programs.ghostty = {
-    enable = true;
-    settings = {
-      inherit font-size;
-      font-family = builtins.elemAt system-config.fonts.fontconfig.defaultFonts.monospace 0; 
-    };
-  };
-}
