75211889227c8628007d7fef95ff362b845a332f
Tatesa Uradnik <taduradnik@proton.me>
2025-05-16 10:09:13 +0200

    215 current  2025-05-16 10:09:12  24.11.20250514.5d73626  6.14.6-zen1                          *

--- system/packages.nix
+++ system/packages.nix
@@ -17,7 +17,7 @@
     evtest
     mdbook
     mpv
-    trashy
+    trash-cli
     (pkgs.writeScriptBin "scrollback" ''${builtins.readFile ../assets/scrollback.sh}'')
     (pkgs.writeScriptBin "nrb" ''${builtins.readFile ../assets/update.nu}'')
     (pkgs.writeScriptBin "cfg" ''${builtins.readFile ../assets/configure.nu}'')
