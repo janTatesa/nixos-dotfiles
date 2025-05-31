3d6fb149ace9c9cb1afae659a04d1e145d1b1274
Tatesa Uradnik <taduradnik@proton.me>
2025-05-31 20:34:46 +0200

    [37m256 24.11.20250523.f09dede 6.14.7-zen1

--- home-manager-shared/starship.nix
+++ home-manager-shared/starship.nix
@@ -5,7 +5,12 @@
     settings = {
       add_newline = false;
       format = "$directory$git_branch$git_commit$git_state$git_metrics$git_status$line_break$battery$nix_shell$character";
-      right_format = "$cmd_duration";
+      right_format = "$custom $cmd_duration";
+      custom.mommy = {
+        command = "mommy -1 -s $env.LAST_EXIT_CODE";
+        when = "true";
+      };
+
       nix_shell = {
         style = "lavender";
         format = "[$state(($name))]($style)";
