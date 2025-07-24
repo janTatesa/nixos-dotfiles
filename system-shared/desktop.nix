00d44283085fd412670e3fc4530178d91e67630e
Tatesa Uradnik <taduradnik@proton.me>
2025-07-24 12:14:33 +0200

    Generation

--- system-shared/desktop.nix
+++ system-shared/desktop.nix
@@ -11,10 +11,6 @@
   };
 
   gtk.iconCache.enable = true;
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-  };
 
   services.keyd = {
     enable = true;
