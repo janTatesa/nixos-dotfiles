16eb8ba1a665c8a848a89f353253741327a6b5d6
Tatesa Uradnik <taduradnik@gmail.com>
2025-09-22 16:24:00 +0200

    update

--- system/packages.nix
+++ system/packages.nix
@@ -18,7 +18,7 @@
     # oxikcde
     prismlauncher
     lutris
-    (pkgs.writeShellScriptBin "wine" "${pkgs.wine64} $@")
+    (pkgs.writeShellScriptBin "wine" "${pkgs.wine64}/bin/wine $@")
     scripts
     obsidian
   ];
