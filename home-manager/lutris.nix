8f297408129ba12157d54f757bb21123f1dd791b
Tatesa Uradnik <taduradnik@gmail.com>
2025-12-23 23:56:02 +0100

    fix: again

--- home-manager/lutris.nix
+++ home-manager/lutris.nix
@@ -4,16 +4,6 @@
     enable = true;
     defaultWinePackage = pkgs.proton-ge-bin;
     protonPackages = [ pkgs.proton-ge-bin ];
-    winePackages = with pkgs; [
-      wineWow64Packages.full
-      wineWowPackages.stagingFull
-    ];
-    extraPackages = with pkgs; [
-      winetricks
-      gamescope
-      gamemode
-      mangohud
-      umu-launcher
-    ];
+    winePackages = [ pkgs.wineWow64Packages.full ];
   };
 }
