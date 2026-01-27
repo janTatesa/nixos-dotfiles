a30c51bf235538e810c260837ec774726a10834d
Tatesa Uradnik <taduradnik@gmail.com>
2026-01-27 11:30:08 +0100

    fix: fix lspmux

--- home-manager/helix/languages.nix
+++ home-manager/helix/languages.nix
@@ -29,7 +29,7 @@
         args = [ "lsp" ];
       };
 
-      rust-analyzer.command = "${pkgs.lspmux}";
+      rust-analyzer.command = "${pkgs.lspmux}/bin/lspmux";
       rust-analyzer.config = {
         check = {
           command = "clippy";
